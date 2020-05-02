80bcb3f450452ef8e119caf55a910586ee08a703
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-05-02 15:40:42 -0400

    pushing...

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("shinto_react/build"));
 
 app.use(require("./routes"));
 
-// app.use(auth.handleErrors);
+app.use(auth.handleErrors);
 
 // db.sequelize.sync();
 // app.listen(process.env.PORT || 8080, () => {

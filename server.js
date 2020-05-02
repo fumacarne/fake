ca1a82e03491f7e10f93345b2acc2bee1859538b
Byron Tottaram <byron.tottaram@alumni.stonybrook.edu>
2020-05-01 23:25:52 -0400

    pushing changges

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+const path = require("path");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "./shinto_react/public");
 
 require("dotenv").config();
 const auth = require("./utils/auth");
 const db = require("./models");
 
-var PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
 
 app.use(cors());
 app.use(morgan("dev"));
